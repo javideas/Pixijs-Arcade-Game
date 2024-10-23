d153d115f75c427d426e785ecb872040b7faad3a
Javier Moreno <109022459+javideas@users.noreply.github.com>
2024-10-23 17:39:26 +0300

    featWip: decks resizing refactoring

--- src/core/tab.ts
+++ src/core/tab.ts
@@ -1,43 +1,9 @@
 import { Container, Graphics } from 'pixi.js';
 const defaultColor = "black";
 
-function getDefaultDimensions(
-    pivotMode: number = 0,
-    posX: number = 0,
-    posY: number = 0,
-    width: number = 0,
-    length: number = 0
-) {
-    const height = window.innerHeight;
-    let frameL, frameR;
-
-    if (pivotMode === 0) {
-        // Growing from left to right
-        frameL = -height * 9 / 32 + posX;
-        frameR = height * 9 / 16 + width;
-    } else {
-        // Growing from right to left
-        // -this.screen.frameL * 2 - (this.screen.frameL * (0.04 * 2))
-        // frameL = 
-        frameL = -height * 9 / 32 - posX - width;
-        frameR = height * 9 / 16 + width;
-    }
-    
-    const frameT = -height / 2 + length;
-    const frameB = height + length;
-    
-    return {
-        pivotMode: pivotMode,
-        frameL: frameL,
-        frameR: frameR,
-        frameT: frameT,
-        frameB: frameB,
-        posX: width / 2,
-        posY: height / 2,
-    };
-}
-
 export class Tab extends Container {
+    private bgShape: Graphics;
+
     constructor(
         bgShapeColor: string = defaultColor,
         pivotMode: number = 0,
@@ -47,17 +13,12 @@ export class Tab extends Container {
         length: number = 0
     ) {
         super();
+
         this.bgShape = new Graphics();
         this.bgShape.color = bgShapeColor;
         this.addChild(this.bgShape);
         
-        this.draw(bgShapeColor, pivotMode, posX, posY, width, length, );
-    }
-
-    public setTracker(parent: Container, property: string = "x") {
-        this.trackerObject = parent;
-        this.trackerValue = this.trackerObject[property];
-        console.log(this.trackerValue);
+        this.draw(bgShapeColor, pivotMode, posX, posY, width, length);
     }
     
     public draw(
@@ -68,20 +29,47 @@ export class Tab extends Container {
         width: number = 0,
         length: number = 0
     ) {
-        // this.width = width;
         this.x = window.innerWidth / 2;
         this.y = window.innerHeight / 2;
 
-        // Calculate frames based on pivotMode and width
-        const dimensions = getDefaultDimensions(pivotMode, posX, posY, width, length);
-        this.frameL = dimensions.frameL;
-        this.frameR = dimensions.frameR;
-        this.frameT = dimensions.frameT;
-        this.frameB = dimensions.frameB;
+        this.calcDimensions(pivotMode, posX, posY, width, length);
 
         this.bgShape.clear();
         this.bgShape.beginFill(bgShapeColor);
         this.bgShape.drawRect(this.frameL, this.frameT, this.frameR, this.frameB);
         this.bgShape.endFill();
+    }
+
+    calcDimensions(
+        pivotMode: number = 0,
+        posX: number = 0,
+        posY: number = 0,
+        width: number = 0,
+        length: number = 0
+    ) {
+        const height = window.innerHeight;
+        let frameL, frameR;
+    
+        if (pivotMode === 0) {
+            // Growing from left to right
+            frameL = -height * 9 / 32 + posX;
+            frameR = height * 9 / 16 + width;
+        } else {
+            // Growing from right to left
+            // -this.screen.frameL * 2 - (this.screen.frameL * (0.04 * 2))
+            // frameL = 
+            frameL = -height * 9 / 32 - posX - width;
+            frameR = height * 9 / 16 + width;
+        }
+        
+        const frameT = -height / 2 + length;
+        const frameB = height + length;
+        
+        this.frameL = frameL;
+        this.frameR = frameR;
+        this.frameT = frameT;
+        this.frameB = frameB;
+        this.posX = width / 2;
+        this.posY = height / 2;
     }
 }
--- src/scenes/battle.ts
+++ src/scenes/battle.ts
@@ -35,9 +35,8 @@ export default class Battle {
         
         this.deckL = new Deck();
         this.app.stage.addChild(this.deckL);
-
+        
         this.resize();
-        this.deckR.setTracker(this.screen, "frameR");
     }
 
     /** Resize the elements on the stage */
