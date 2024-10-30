72f240cfc1dd0ec714bf693ea483aef28ab3386e
Javier Moreno <109022459+javideas@users.noreply.github.com>
2024-10-30 17:25:05 +0200

    Updates

--- preload.js
+++
@@ -1,5 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electron', {
-    onAltF11Pressed: (callback) => ipcRenderer.on('altf11-pressed', callback)
-});
